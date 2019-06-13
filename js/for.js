8b6a4a7882ea056a87b1cbb6f4134f3c3bfc828c
Eduardo Ruiz <eduruiz333@gmail.com>
2019-06-13 18:50:51 -0300

    envio dos primeiros arquivos de exercícios

---
+++ js/for.js
@@ -0,0 +1,14 @@
+let a = 5
+let b = 12
+let c = 7
+let d = 122
+let e = 182
+
+let all = [a, b, c, d, e]
+all.push(777);
+
+for (i = 0; i < all.length; i++) {
+    console.log(all[i]);
+    
+}
+
---
+++ js/hash.js
@@ -0,0 +1,19 @@
+let hash = {
+    nome: 'Eduardo',
+    idade: 47,
+    genero: 'Masculino'
+}
+
+hash.cpf = 123123123;
+
+console.log('O nome é: ' + hash.nome); // FORMA CONCATENADA
+console.log(`A idade é ${hash.idade} e o gênero é ${hash.genero}`); // FORMA TELMPATE STRING
+console.log(`O CPF inserido foi: ${hash.cpf}`);
+console.log(`O tipo da variável hash é '${typeof(hash)}'`);
+
+// PARA CONJUNTOS USAMOS ARRAY
+// PARA DADOS ESTRUTURADOS USAMOS HASH
+// JASON UTILIZA FORMATAÇÃO HASH
+
+
+
